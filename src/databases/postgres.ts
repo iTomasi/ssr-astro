af8c39944b0679266dd9f57ab9a077028959c61c
Tomas Duclos <duclosiw@gmail.com>
2022-04-15 16:26:46 -0400

    a

--- src/databases/postgres.ts
+++ src/databases/postgres.ts
@@ -3,11 +3,11 @@ import pg from "pg"
 
 const { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_DB_NAME } = import.meta.env;
 
-const HOST = POSTGRES_HOST || "";
-const PORT = POSTGRES_PORT || 5432;
-const USERNAME = POSTGRES_USERNAME || "postgres"
-const PASSWORD = POSTGRES_PASSWORD || "password"
-const DB_NAME = POSTGRES_DB_NAME || "postgres"
+const HOST = POSTGRES_HOST || process.env.POSTGRES_HOST || "";
+const PORT = POSTGRES_PORT || process.env.POSTGRES_PORT || 5432;
+const USERNAME = POSTGRES_USERNAME || process.env.POSTGRES_USERNAME || "postgres"
+const PASSWORD = POSTGRES_PASSWORD || process.env.POSTGRES_PASSWORD || "password"
+const DB_NAME = POSTGRES_DB_NAME || process.env.POSTGRES_DB_NAME || "postgres"
 
 export const postgres = new Sequelize(`postgres://${USERNAME}:${PASSWORD}@${HOST}:${PORT}/${DB_NAME}`, {
   dialect: "postgres",
