b5e0327e0f512ad262b171760eca30b8bf891c62
Tomas Duclos <duclosiw@gmail.com>
2022-04-29 16:50:23 -0400

    fixing auth wrapper height for scroll

--- src/components/auth/Wrapper.tsx
+++ src/components/auth/Wrapper.tsx
@@ -9,12 +9,14 @@ interface IWrapperProps {
 function Wrapper({ title, children, onSubmit }: IWrapperProps) {
   return (
     <div className="iw-fixed iw-top-16 iw-right-0 iw-bottom-0 iw-left-0 iw-grid iw-place-items-center iw-z-10">
-      <div className="iw-w-11/12 iw-max-w-xl">
-        <h1 className="iw-text-3xl iw-mb-8 iw-text-center">{title}</h1>
+      <div className="iw-w-full iw-max-h-[100vh] iw-overflow-y-auto iw-py-16">
+        <div className="iw-w-11/12 iw-max-w-xl iw-mx-auto">
+          <h1 className="iw-text-3xl iw-mb-8 iw-text-center">{title}</h1>
 
-        <form className="iw-bg-stone-800 iw-p-4 iw-rounded" onSubmit={onSubmit}>
-          {children}
-        </form>
+          <form className="iw-bg-stone-800 iw-p-4 iw-rounded" onSubmit={onSubmit}>
+            {children}
+          </form>
+        </div>
       </div>
     </div>
   )
