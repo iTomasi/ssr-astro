e1c341cd17358df4bf5b66cfc2fde69d07331ab2
Tomas Duclos <duclosiw@gmail.com>
2022-04-29 16:52:56 -0400

    add: more padding

--- src/components/auth/Wrapper.tsx
+++ src/components/auth/Wrapper.tsx
@@ -9,7 +9,7 @@ interface IWrapperProps {
 function Wrapper({ title, children, onSubmit }: IWrapperProps) {
   return (
     <div className="iw-fixed iw-top-16 iw-right-0 iw-bottom-0 iw-left-0 iw-grid iw-place-items-center iw-z-10">
-      <div className="iw-w-full iw-max-h-[100vh] iw-overflow-y-auto iw-py-16">
+      <div className="iw-w-full iw-max-h-[100vh] iw-overflow-y-auto iw-py-20">
         <div className="iw-w-11/12 iw-max-w-xl iw-mx-auto">
           <h1 className="iw-text-3xl iw-mb-8 iw-text-center">{title}</h1>
 
